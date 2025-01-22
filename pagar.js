f185fffea3f7c91df97904bc391b7c0eb81224ba
Lannay Itauana <lannayitauana.c.caldas@gmail.com>
2025-01-21 21:19:25 -0300

    Add files via upload

--- pagar.js
+++ pagar.js
@@ -16,15 +16,39 @@ function salvarPagamento(event){
     usuario.nome = document.getElementById('nome').value;
     usuario.email = document.getElementById('email').value;
     usuario.telefone = document.getElementById('telefone').value;
+
+    const metodo = document.querySelector('input[name="metodo"]:checked');
+    if(metodo){
+        usuario.metodoPagamento = metodo.value;
+    }
+
+    if(usuario.metodoPagamento === 'cartao'){
+        usuario.detalhesCartao.numero = document.getElementById('numero-cartao').value;
+        usuario.detalhesCartao.validade = document.getElementById('validade').value;
+        usuario.detalhesCartao.cvv = document.getElementById('cvv').value;
+    }
+
+    //Exibe dados coletados no console para simulação
+    console.log('Dados do usuario:', usuario);
+
+    //Redeirecionar para a página de confirmação
+    window.location.href = 'confirmação.html';
+
 }
 
-const metodo = document.querySelector('input[name="metodo"]:checked');
-if(metodo){
-    usuario.metodoPagamento = metodo.value;
+//Função para exibir mensagem de confirmação
+function carregarConfirmação(){
+    const mensagem = document.getElementById('mensagem-confirmação');
+    if (mensagem && usuario.none) {
+        mensagem.textContent = 'Obrigado pelo pagamento, ${usuario.none}!'
+    }
 }
 
-if(usuario.metodoPagamento === 'cartao'){
-    usuario.detalhesCartao.numero = document.getElementById('numero-cartao').value;
-    usuario.detalhesCartao.validade = document.getElementById('validade').value;
-    usuario.detalhesCartao.cvv = document.getElementById('cvv').value;
+//Event listener para salvar o pagamento ao enviar o formulário
+const formPagamento = document.querySelector('.pagamento form');
+if (formPagamento) {
+    formPagamento.addEventListener('submit', salvarPagamento)
 }
+
+//Carregar dado sna página de confirmação
+window.addEventListener('DOMContentLoaded', carregarConfirmacao);
